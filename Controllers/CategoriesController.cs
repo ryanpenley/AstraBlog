4d743b45447dc76509e425735728f00f111ea591
Ryan Penley <ryanpenley@gmail.com>
2023-02-23 12:02:56 -0500

    added imageFile to bind on categories controller

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -80,7 +80,7 @@ namespace AstraBlog.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Description,ImageData,ImageType")] Category category)
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,ImageData,ImageType,ImageFile")] Category category)
         {
             if (ModelState.IsValid)
             {
@@ -112,7 +112,7 @@ namespace AstraBlog.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,ImageData,ImageType")] Category category)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,ImageData,ImageType,ImageFile")] Category category)
         {
             if (id != category.Id)
             {
